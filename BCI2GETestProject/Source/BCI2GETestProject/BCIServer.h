4ea68aa38759bf7bc0bf15419c8ccad268a1954a
Aske Aske <adamaske@live.no>
2022-11-15 18:17:21 +0100

    Working with bci

---
+++ BCI2GETestProject/Source/BCI2GETestProject/BCIServer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OSCServer.h"
+#include "BCIServer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BCI2GETESTPROJECT_API UBCIServer : public UOSCServer
+{
+	GENERATED_BODY()
+
+};
